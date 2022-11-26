7006bf85a6502a262e72a03a4e5f7f44d8c221ee
Itamar <itamarlucio19@gmail.com>
2022-11-26 14:03:40 -0300

    Finished project

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 let passwordOne = document.getElementById("password-one")
 let passwordTwo = document.getElementById("password-two")
-var passwordLength = parseInt(document.getElementById("pass-input").value)
+let passwordLength = document.getElementById("pass-input")
+let numberText = document.getElementById("number-text")
+let symbolText = document.getElementById ("symbol-text")
+numberCheck = false
+symbolCheck = false
 
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+let letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+let characters = letters
+
+function numberBtn(){
+  if (numberCheck === false){    
+    numberText.textContent = "X"
+    characters = characters.concat(numbers)
+    numberCheck = true
+    console.log(characters)
+  }
+  else {
+    numberText.textContent = ""
+    numberCheck = false
+    characters = characters.filter(function(rm){
+      return !numbers.includes(rm)
+    })
+    console.log(characters)
+  }
+}
+
+function symbolBtn(){
+  if (symbolCheck === false){    
+    symbolText.textContent = "X"
+    characters = characters.concat(symbols)
+    symbolCheck = true
+  }
+  else {
+    symbolText.textContent = ""
+    symbolCheck = false
+    characters = characters.filter(function(rm){
+      return !symbols.includes(rm)
+    })
+  }
+}
 
 function randomPasswords(){
   password = ""
-  for (i=1;i<16;i++) {
+  for (i=1;i<(parseInt(passwordLength.value)+1);i++) {
     let char = characters[Math.floor(Math.random()*characters.length)]
     password += char
   }
@@ -19,5 +57,12 @@ function GeneratePasswords(){
   passwordTwo.textContent = randomPasswords()
 }
 
-console.log(passwordLength)
-console.log("passwordLength")
+function copyPasswordOne(){
+  passwordOne.select()
+  navigator.clipboard.writeText(passwordOne.value)
+}
+function copyPasswordTwo(){
+  passwordTwo.select()
+  navigator.clipboard.writeText(passwordTwo.value)
+}
+
